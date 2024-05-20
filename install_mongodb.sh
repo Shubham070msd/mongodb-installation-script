9c4f92d110cf5405d4d45b1e2fb23cc9afe8c238
Manjunath Huddar <67737064+Shubham070msd@users.noreply.github.com>
2024-05-20 20:00:12 +0530

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -8,6 +8,9 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
    --dearmor
 
+# Create a list file for MongoDB   
+/etc/apt/sources.list.d/mongodb-org-6.0.list
+
 # Add MongoDB repository to sources.list.d
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
